aaa6026a81dd1a249d0981ca97378d18ead4b450
Karel Blavka <karel.blavka@hardwario.com>
2019-05-21 13:38:03 +0200

    Update vue.config.js  add build options

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,38 @@
 module.exports = {
     pluginOptions: {
       electronBuilder: {
-        externals: ['serialport']
+        externals: ['serialport'],
+        builderOptions:{
+          productName: "COOPER Quick",
+          "appId": "com.electron.hardwario-cooper-quick",
+          "artifactName": "cooper-quick-${version}-${os}-${arch}.${ext}",
+          "publish": null,
+          "mac": {
+            "artifactName": "cooper-quick-${version}-macos-${arch}.${ext}",
+            "category": "public.app-category.utilities"
+          },
+          "win": {
+            "artifactName": "cooper-quick-${version}-windows-${arch}.${ext}",
+            "target": [
+              "nsis",
+              "portable"
+            ],
+            "publisherName": "HARDWARIO s.r.o."
+          },
+          "linux": {
+            "artifactName": "cooper-quick-${version}-linux-${arch}.${ext}",
+            "target": [
+              "deb",
+            ],
+            "category": "Utility"
+          },
+          "appImage": {
+            "systemIntegration": "doNotAsk"
+          },
+          "nsis": {
+            "artifactName": "cooper-quick-${version}-${os}-setup-${arch}.${ext}"
+          }
+        }
       }
     }
   }
